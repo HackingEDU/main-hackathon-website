7ee3f6f505441f4cad3fc3b3240a9ab7d7f810bb
Hunter S <dook@stardust.red>
2015-06-30 19:11:05 -0400

    Changed request body post from querystring to full json body

--- routes.js
+++ routes.js
@@ -1,10 +1,6 @@
 var path = require('path');
-var querystring = require("querystring");
 var request = require("request");
-var https = require("https");
-var url = require('url');
 var Promise = require("promise");
-var config = require('./config')
 
 module.exports = {
   home: function(req, res) {
@@ -22,70 +18,33 @@ module.exports = {
     // This is now a proxy to Cloud Code: will probably be better for security
     // Also, all the validation is now handled by Parse servers (cloud code),
     // might be better for server load
-    var post_data = querystring.stringify(req.body);
 
     // AJAX promise definitions
     var validate = new Promise(
       function(resolve, reject) {
-        // Validate fields
-        var val_request = https.request(
-          {
-              method: "POST",
-            hostname: "hackingedu.parseapp.com",
-                path: "/actions/v62110a75095ebf61417a51fff9af9c7f", // Validation AJAX
-             headers: {
-                        "Content-Type": "application/x-www-form-urlencoded",
-                        "Content-Length": post_data.length
-                      }
-          }
-        ).on("response", function httpsResponse(response) {
-            response.setEncoding("utf8");
-            response.on("data", function(chunk) {
-              resolve(chunk);
-            });
-          }
-        ).on("error", function httpsResponse(response) {
-            console.log("POST error...?");
-            reject(response);
+        // Request to validate fields
+        request.post("http://hackingedu.parseapp.com/actions/v62110a75095ebf61417a51fff9af9c7f",
+          { form: req.body },
+          function(err, response, body) {
+            if(err) { reject(err);   }
+            else    { resolve(body); }
           }
         );
-
-        val_request.write(post_data);
-        val_request.end();
       }
     );
 
     var createUser = new Promise(
       function(resolve, reject) {
-        // Validate fields
-        var new_request = https.request(
-          {
-              method: "POST",
-            hostname: "hackingedu.parseapp.com",
-                path: "/actions/n0354d89c28ec399c00d3cb2d094cf093", // New User AJAX
-             headers: {
-                        "Content-Type": "application/x-www-form-urlencoded",
-                        "Content-Length": post_data.length
-                      }
-          }
-        ).on("response", function httpsResponse(response) {
-            response.setEncoding("utf8");
-            response.on("data", function(chunk) {
-              resolve(chunk);
-            });
-          }
-        ).on("error", function httpsResponse(response) {
-            console.log("POST error...?");
-            reject(response);
+        // Request to create a new user
+        request.post("http://hackingedu.parseapp.com/actions/n0354d89c28ec399c00d3cb2d094cf093",
+          { form: req.body },
+          function(err, response, body) {
+            if(err) { reject(err);   }
+            else    { resolve(body); }
           }
         );
-
-        new_request.write(post_data);
-        new_request.end();
       }
     );
-
-
 
     validate.then(
       function handleValidated(retval) {
@@ -102,7 +61,6 @@ module.exports = {
         // retval = { code: ..., message: ..., fields: ...
         //  @code: status code
         //  @message: ...
-        //  @fields: Array of invalid fields, if any
         console.log(retval);
         res.end(retval);
       },
