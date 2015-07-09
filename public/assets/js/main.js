a4d28208e67bdd7f7f6c49359bc3aab9dd1b2564
rrtigga <rrtigga@ucdavis.edu>
2015-07-09 02:39:34 -0700

    nav bar changes and social footer changes

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,22 +1,3 @@
-//modals
-$('#sponsors-modal-link').animatedModal({
-	modalTarget: 'sponsors-modal',
-	animatedIn: 'lightSpeedIn',
-	animatedOut: 'bounceOutLeft'
-});
-
-$('#speakers-modal-link').animatedModal({
-	modalTarget: 'speakers-modal',
-	animatedIn: 'lightSpeedIn',
-	animatedOut: 'bounceOutLeft'
-});
-
-$('#workshops-modal-link').animatedModal({
-	modalTarget: 'workshops-modal',
-	animatedIn: 'lightSpeedIn',
-	animatedOut: 'bounceOutLeft'
-});
-
 // Smooth scrolling plugin by Chris Coiyer
 // Source: http://css-tricks.com/snippets/jquery/smooth-scrolling/
 $('a[href*=#]:not([href=#])').click(function() {
